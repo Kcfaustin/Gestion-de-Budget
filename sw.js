f8164b683c969d41df5cf00cb14a51beb767e80e
Faustin AGOHOUNDJE <54989076+Kcfaustin@users.noreply.github.com>
2026-05-05 19:39:30 +0100

    fix: update service worker cache version to v3.5 and adjust export button styles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'budgetsplit-v3.4';
+const CACHE = 'budgetsplit-v3.5';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.svg'];
 
 self.addEventListener('install', e => {
