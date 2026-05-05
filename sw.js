bad48a1bf583c1b33a9551627b272e2c7ca718a6
Faustin AGOHOUNDJE <54989076+Kcfaustin@users.noreply.github.com>
2026-05-05 12:09:13 +0100

    fix: update service worker cache version to v3.3 and enhance install prompt functionality

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'budgetsplit-v3';
+const CACHE = 'budgetsplit-v3.3';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.svg'];
 
 self.addEventListener('install', e => {
